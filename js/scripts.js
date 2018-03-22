49ff96f7c9b044682e71b0e8cd0b9893169c34a3
Kamil Makoś <makos.kamil@o2.pl>
2018-03-22 23:52:22 +0100

    add_files

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+// zacznijmy pisać nowy kod:
+
+function getTriangleArea(a, h) 
+{
+    if (!(a>0) || !(h>0)) 
+    {
+        console.log('Nieprawidłowe dane')
+    }
+    
+    else 
+    {
+    return getTriangleArea = a*h/2;
+    }
+}
+
+console.log(getTriangleArea(10,6))
